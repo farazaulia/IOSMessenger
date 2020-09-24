ac6ee501fc466210c40e8965698bc7e0739cf0c2
Farazaulia <raz.muhammad14@gmail.com>
2020-09-24 11:53:28 +0700

    Initial Commit

---
+++ IOSMessenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOSMessenger
+//
+//  Created by Faraz Muhammad Aulia on 24/09/20.
+//  Copyright © 2020 Faraz Muhammad Aulia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
